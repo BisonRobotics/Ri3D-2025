45266b2345baa37be2c440152ee080540bc1a537
dylanrattai <d_rattai@yahoo.com>
2025-01-05 13:51:16 -0600

    Git didnt add the new files to my commit (again)

---
+++ src/main/java/frc/robot/Commands/DefaultWristCommand.java
@@ -0,0 +1,30 @@
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.Subsystems.WristSubsystem;
+
+public class DefaultWristCommand extends Command{
+    
+    private WristSubsystem wristSubsystem;
+
+    public DefaultWristCommand(WristSubsystem wristSubsystem)
+    {
+        this.wristSubsystem = wristSubsystem;
+
+        addRequirements(wristSubsystem);
+    }
+
+    @Override
+    public void execute()
+    {
+        if (!wristSubsystem.getInTolerance())
+        {
+            wristSubsystem.setPosition(Constants.WristConstants.WRIST_PARK_ANGLE);
+        }
+        else
+        {
+            wristSubsystem.stopWrist();
+        }
+    }
+}
---
+++ src/main/java/frc/robot/Commands/ElevatorToCommand.java
@@ -0,0 +1,45 @@
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.Subsystems.ElevatorSubsystem;
+import frc.robot.Subsystems.WristSubsystem;
+
+public class ElevatorToCommand extends Command{
+    
+    private ElevatorSubsystem elevatorSubsystem;
+    private WristSubsystem wristSubsystem;
+    private double elevatorHeight;
+    private double wristAngle;
+
+    public ElevatorToCommand(ElevatorSubsystem elevatorSubsystem, WristSubsystem wristSubsystem, double elevatorHeight, double wristAngle)
+    {
+        this.elevatorSubsystem = elevatorSubsystem;
+        this.wristSubsystem = wristSubsystem;
+        this.elevatorHeight = elevatorHeight;
+        this.wristAngle = wristAngle;
+
+        addRequirements(elevatorSubsystem, wristSubsystem);
+    }
+
+    @Override
+    public void initialize()
+    {
+        elevatorSubsystem.setPostion(elevatorHeight);
+        wristSubsystem.setPosition(wristAngle);
+    }
+
+    @Override
+    public void execute()
+    {
+        elevatorSubsystem.setPostion(elevatorHeight);
+        wristSubsystem.setPosition(wristAngle);
+    }
+
+    @Override
+    public void end(boolean interrupted)
+    {
+        wristSubsystem.setPosition(Constants.WristConstants.WRIST_PARK_ANGLE);
+        elevatorSubsystem.setPostion(Constants.ElevatorConstants.ELEVATOR_PARK_HEIGHT);
+    }
+}
