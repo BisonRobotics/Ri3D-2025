168d2cee8af0513debb59a903d53ed84edc769d2
dylanrattai <d_rattai@yahoo.com>
2025-01-05 10:16:09 -0600

    Created wrist subsystem and manipulator subsystem. Wrist subsystem's PID and feed forward need to be tuned.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,5 +12,32 @@ public class Constants
         public static final int controller_port = 0;
         public static final double stickDeadband = 0.1; // TODO: Test me
     }
+
+    public static final class ManipulatorConstants {
+        public static final int BOTTOM_MANIPULATOR_MOTOR_ID = 100; // !!!!!! needs value !!!!!!
+        public static final int MIDDLE_MANIPULATOR_MOTOR_ID = 100; // !!!!!! needs value !!!!!!
+        public static final int TOP_MANIPULATOR_MOTOR_ID = 100; // !!!!! needs value !!!!!
+
+        public static final double INTAKE_SPEED = 0.3;
+        public static final double HOLD_SPEED = 0.01;
+        public static final double PLACE_SPEED = 0.2;
+    }
+
+    public static final class WristConstants {
+        public static final int WRIST_MOTOR_ID = 100; // !!!!! needs value !!!!!
+
+        // PID Constants TODO: tune
+        public static final double WRIST_PID_TOLERANCE = 0.1;
+        public static final double WRIST_kP = 0;
+        public static final double WRIST_kI = 0;
+        public static final double WRIST_kD = 0;
+        public static final double WRIST_kS = 0;
+        public static final double WRIST_kG = 0;
+        public static final double WRIST_kV = 0;
+
+        // Limits
+        public static final double WRIST_LIMIT_TOP = 0;
+        public static final double WRIST_LIMIT_BOTTOM = 0;
+    }
 }
 
