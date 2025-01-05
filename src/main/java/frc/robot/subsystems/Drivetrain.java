0d5da0d44c8d24be1123e7999d253e9217cddb82
Sam Stockstrom <144393153+sstock2005@users.noreply.github.com>
2025-01-04 21:52:46 -0600

    Replace .setInverted with SparkMaxConfig
    Needs to be tested!

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,8 +1,9 @@
 package frc.robot.subsystems;
 
-import java.util.function.DoubleSupplier;
-
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkBase.PersistMode;
 
 import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -22,7 +23,13 @@ public class Drivetrain extends SubsystemBase
     {
         m_leftMotor = new SparkMax(Constants.Drivetrain.leftmotor_port, MotorType.kBrushless);
         m_rightMotor = new SparkMax(Constants.Drivetrain.rightmotor_port, MotorType.kBrushless);
-        m_rightMotor.setInverted(true); // TODO: Implement SparkBaseConfig
+
+        // .setInverted is deprecated, using SparkMaxConfig instead
+        // TODO: Test this configuration
+        SparkMaxConfig rightConfig = new SparkMaxConfig();
+        rightConfig.inverted(true);
+        m_rightMotor.configure(rightConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+        
         m_robotDrive = new DifferentialDrive(m_leftMotor::set, m_rightMotor::set);
         SendableRegistry.addChild(m_robotDrive, m_leftMotor);
         SendableRegistry.addChild(m_robotDrive, m_rightMotor);
