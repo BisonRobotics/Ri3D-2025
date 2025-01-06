fcf443d7dfc586a56ba1bae9efec63c1324ada46
Sam Stockstrom <144393153+sstock2005@users.noreply.github.com>
2025-01-06 09:38:35 -0600

    + Elevator and Wrist have a park button now

--- src/main/java/frc/robot/Commands/MindControlCommand.java
+++ src/main/java/frc/robot/Commands/MindControlCommand.java
@@ -4,6 +4,7 @@ import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.Constants;
 import frc.robot.Subsystems.ElevatorSubsystem;
 import frc.robot.Subsystems.WristSubsystem;
@@ -15,14 +16,16 @@ public class MindControlCommand extends Command
     private WristSubsystem m_wrist;
     private BooleanSupplier m_button3pressed;
     private BooleanSupplier m_button4pressed;
+    private BooleanSupplier m_button10pressed;
 
-    public MindControlCommand(DoubleSupplier pov, BooleanSupplier button3pressed, BooleanSupplier button4pressed, ElevatorSubsystem elevator, WristSubsystem wrist)
+    public MindControlCommand(DoubleSupplier pov, BooleanSupplier button3pressed, BooleanSupplier button4pressed, BooleanSupplier button10pressed, ElevatorSubsystem elevator, WristSubsystem wrist)
     {
         m_pov = pov;
         m_elevator = elevator;
         m_wrist = wrist;
         m_button3pressed = button3pressed;
         m_button4pressed = button4pressed;
+        m_button10pressed = button10pressed;
     }
 
     @Override
@@ -59,6 +62,11 @@ public class MindControlCommand extends Command
             if (m_button4pressed.getAsBoolean())
             {
                 new ElevatorToCommand(m_elevator, m_wrist, Constants.ElevatorConstants.PICKUP_ALGAE_L2, Constants.ElevatorConstants.PICKUP_ALGAE_L2).wait();
+            }
+
+            if (m_button10pressed.getAsBoolean())
+            {
+                new ParallelCommandGroup(new DefaultElevatorCommand(m_elevator), new DefaultWristCommand(m_wrist)).wait();
             }
         }
         catch (InterruptedException e)
